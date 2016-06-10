656ecf722c684fd6c75068d2c29ea286d4adedea
Tristan B. Kildaire <deavmi@users.noreply.github.com>
2016-06-10 23:39:42 +0200

    Update PostMan.java

--- PostMan.java
+++ PostMan.java
@@ -1,11 +1,12 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.FileWriter;
+import java.until.Scanner;
 
 public class PostMan
 {
 
-   public static String getPostID(String data)
+   public static String getPostID(String data) //GetPostID
    {
       return data+Math.random()*data.length(); //random pous
    }
@@ -28,6 +29,37 @@ public class PostMan
       {
          System.out.println("Error saving post file.");
       }
+   
+   }
+
+   public static void eval(String data)
+   {
+   
+      Scanner packetReader = new Scanner(data);
+      
+      String command = "";
+      
+      boolean cmdDone = false;
+      while(packetReader.hasNextLine())
+      {
+         if (cmdDone == false)
+         {
+            command = packetReader.nextLine();
+            cmdDone = true;
+         }
+         else
+         {
+            if (command.equalsIgnoreCase("post"))
+            {
+               if (command.equalsIgnoreCase("post"))
+               {
+                   post(packetReader.next(),packetReader.next(),packetReader.next());
+               }
+            }
+         }
+      }
+      
+      packetReader.close();
    
    }
 
