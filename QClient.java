2206fbc2487e8c0332adecc2da98713bf27ad3eb
Tristan B. Kildaire <deavmi@users.noreply.github.com>
2016-06-10 22:04:27 +0200

    Update QClient.java

--- QClient.java
+++ QClient.java
@@ -1,15 +1,30 @@
 //QNET client lib
+import java.net.Socket;
+import java.net.InetAddress;
+import java.io.IOException;
 
 public class QClient
 {
 
    private int port;
+   private InetAddress server;
 
-   public QClient(int port)
+   public QClient(InetAddress server, int port)
    {
-   
       this.port = port;
+      this.server = server;
+   }
    
+   public void post()
+   {
+      try
+      {
+         Socket sock = new Socket(server, port);
+      }
+      catch (IOException err)
+      {
+         System.out.println("Error connecting to server: " + err);           
+      }
    }
 
 }
