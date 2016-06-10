2ecd1162a7b0faa00ed09e8b1dad4846c47fbc17
Tristan B. Kildaire <deavmi@users.noreply.github.com>
2016-06-10 23:08:35 +0200

    Create PostMan.java

---
+++ PostMan.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+
+public class PostMan
+{
+
+   public static String getPostID(String data)
+   {
+      return data+Math.random()*data.length(); //random pous
+   }
+
+   public static void addPost(String author, String title, String body)
+   {
+      try
+      {
+         PrintWriter writer = new PrintWriter(new FileWriter(getPostID(author+title+body)));
+         
+         writer.println();
+         
+         
+         writer.close();
+      
+      }
+      catch(IOException err)
+      {
+         System.out.println("Error saving post file.");
+      }
+   
+   }
+
+}
