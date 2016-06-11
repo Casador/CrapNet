5ea45dadfea13224c8ac8a7614ad5e95317ec0f6
Tristan B. Kildaire <deavmi@users.noreply.github.com>
2016-06-11 12:29:46 +0200

    Update qnetd.java

--- qnetd.java
+++ qnetd.java
@@ -2,7 +2,7 @@
 //The QNet Server and Exchanger frontend
 
 import java.io.IOException;
-import java.until.Scanner;
+import java.util.Scanner;
 import java.io.File;
 
 public class qnetd
@@ -11,61 +11,58 @@ public class qnetd
    public static void main (String[] args)
    {
       
-      int argLen = args.len();
-      int count = 0;
-      for (int argI = 0; argI < argLen; argI++)
+      if (args.len() == 0)
       {
-           if (count == 0)
-           {
-               if (args[argI].equals("--server"))
-               {
-                   startup();
-                   break;
-               }
-               else if (args[argI].equals("--help"))
-               {
-                   showHelp();
-                   break;
-               }
-               else
-               {
-                   System.out.println("Command \" + args[argI] + "\" not found.");
-                   System.exit(1);
-               }
-           }
-           count++;
+         startup();
       }
-
-      public static void startup()
+      else
       {
-          System.out.println("[qnetd] Info: Starting QServer...");
-          System.out.println("[qnetd] Info: I shall hang here till thee control-c-mee.");
-          try
-          {
-             into port = 0;
-             Scanner reader = new Scanner(new File("qnet_server.conf"));
-             while (reader.hasNextLine())
-             {
-                Scanner f = new Scanner(reader.nextLine()).useDelimiter("=");
-                f.next();
-                port = f.nextInt();
-                f.close();
-             }
-             reader.close();
-             QServer.listen(port);
-          }
-          catch (IOException err)
-          {
-             System.out.println("[qnetd] Error: Config file \"qnet_server.conf\" not found or could not be opened.");
-             System.out.println("[qnetd] Reason: " + err.getMessage());
-             System.exit(2);
-          }
-      }
+         if (args[0].equals("--server"))
+         {
+             startup();
+         }
+         else if (args[0].equals("--help"))
+         {
+             showHelp();
+         }
+         else
+         {
+             System.out.println("Command \"" + args[0] + "\" not found.");
+             System.exit(1);
+         }
       
+      }
+   }
+
+   public static void startup()
+   {
+       System.out.println("[qnetd] Info: Starting QServer...");
+       System.out.println("[qnetd] Info: I shall hang here till thee control-c-mee.");
+       try
+       {
+           int port = 0;
+           Scanner reader = new Scanner(new File("qnet_server.conf"));
+           while (reader.hasNextLine())
+           {
+               Scanner f = new Scanner(reader.nextLine()).useDelimiter("=");
+               f.next();
+               port = f.nextInt();
+               f.close();
+           }
+           reader.close();
+           QServer.listen(port); //add multithreading here
+      }
+      catch (IOException err)
+      {
+          System.out.println("[qnetd] Error: Config file \"qnet_server.conf\" not found or could not be opened.");
+          System.out.println("[qnetd] Reason: " + err.getMessage());
+          System.exit(2);
+      }
    }
 
    public static void showHelp()
    {
        System.out.println("You had one job Matthew Seamen Carter. And you fucked it up.");
    }
+
 }
